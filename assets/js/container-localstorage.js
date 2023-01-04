d948dc6cb64b43db5d36639d6bf69a5c88f563dc
CZ-RBelo <jr.belo@hotmail.com>
2023-01-04 20:32:53 +0000

    The main functions call code moved from container-localstorage js file to main js file

--- assets/js/container-localstorage.js
+++ assets/js/container-localstorage.js
@@ -1,69 +1,7 @@
-// Spotify & Wikipedia API Keys
-
-const wikipediaAPIKey = "";
-const spotifyAPIKey = "c1ec80a3b6msh5a59fdbd8715fddp1e8eb2jsn5706ec8ba70f";
-
 var pastUserSearch = $("#history");
-
-// function to hide the invalid user search alert
-function alertClose() {
-  document.getElementById("ms-alert").style.visibility = "hidden";
-}
-
-// clear the local storage and the searches history
-function clearHistory() {
-  // if is visible then hide the invalid user search alert
-  alertClose();
-  // clear the local storage
-  localStorage.clear();
-  var historyEl = document.getElementById("history");
-  // clear the searches history
-  historyEl.innerHTML = "";
-  return;
-}
 
 // Display search history function
 displaySearchHistory();
-
-// Function to check if the user search return any value
-function checkSearch(userSearch) {
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url:
-      "https://spotify23.p.rapidapi.com/search/?q=" +
-      userSearch +
-      "&type=multi&offset=0&limit=10&numberOfTopResults=5",
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": spotifyAPIKey,
-      "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
-    },
-  };
-
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    // Check the response length
-    if (response.artists.totalCount === 0) {
-      // Display an alert for invalid user search
-      document.getElementById("ms-alert").style.visibility = "visible";
-    } else {
-      // Save into the local storage the user search
-      saveSearch(userSearch);
-
-      // Display into the search history the new user search
-      displaySearchHistory();
-
-      // Display Wikipedia data into HTML
-      // searchWikipedia(userSearch);
-
-      // Display Spotify data into HTML
-      //  searchSpotify(userSearch);
-      /*      };
-          });*/
-    }
-  });
-}
 
 // Save into the local storage the user search
 function saveSearch(userSearch) {
