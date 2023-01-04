3cef7653ae6fefc3f19124fc755655b7edaf14a9
JackLabukas <labukasgediminas@gmail.com>
2023-01-04 19:23:44 +0000

    updated logic

--- assets/js/container-spotify.js
+++ assets/js/container-spotify.js
@@ -19,8 +19,29 @@ function searchArtist(artist) {
   });
 }
 
-$("#select-artist").on("click", function (event) {
+$("#search-button").on("click", function (event) {
   event.preventDefault();
-  var inputArtist = $("#artist-input").val().trim();
+  var inputArtist = $("#artist-input").val();
   searchArtist(inputArtist);
+});
+
+$("#trendingMain").text("hello");
+$("#trendingSide").text("hello");
+
+const settings = {
+  async: true,
+  crossDomain: true,
+  url:
+    "https://spotify23.p.rapidapi.com/search/?q=" +
+    artist +
+    "&type=multi&offset=0&limit=10&numberOfTopResults=5",
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c1ec80a3b6msh5a59fdbd8715fddp1e8eb2jsn5706ec8ba70f",
+    "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
 });
