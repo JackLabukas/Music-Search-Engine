81c88db21b0789e2aab60914049e17327db70ecd
CZ-RBelo <jr.belo@hotmail.com>
2023-01-05 16:26:54 +0000

    Team javascript file added

---
+++ assets/js/container-team.js
@@ -0,0 +1,60 @@
+function displayTeam() {
+
+    var team =
+        [
+            {
+                name: 'Amal Hussein',
+                linkedin: '#',
+                github: '#',
+                email: 'amula9986.ah@gmail.com',
+                tasks: 'Implementing Wikipedia Functionality'
+            },
+            {
+                name: 'Jack Labukas',
+                linkedin: '#',
+                github: '#',
+                email: 'labukasgediminas@gmail.com',
+                tasks: 'Implementing Spotify Functionality'
+            },
+            {
+                name: 'Kassi',
+                linkedin: '#',
+                github: '#',
+                email: 'kassy.g.e@hotmail.com',
+                tasks: 'Presenting • Assisting with YouTube API • Generating ReadMe file • Poject management • Team organisation'
+            },
+            {
+                name: 'Rui Belo',
+                linkedin: 'https://linkedin.com/in/ruibelo',
+                github: 'https://github.com/CZ-RBelo/',
+                email: 'jr.belo@hotmail.com',
+                tasks: 'Bootstrap • UI Design • Implementing Code/ Refactoring • Combining APIs & Local Storage'
+            }
+        ]   
+
+    for (var i = 0; i < team.length; i++) {
+
+        // Build the html card
+        var teamCard = $(`
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title mark">${team[i].name}</h5>
+                    <div class="footer-basic">
+                        <div class="social">
+                            <a href="${team[i].linkedin}" target="_blank"><i class="icon ion-social-linkedin"></i></a>
+                            <a href="${team[i].github}" target="_blank"><i class="icon ion-social-github"></i></a>
+                            <a href="${team[i].email}" target="_blank"><i class="icon ion-email"></i></a>
+                        </div>
+                    </div>
+                    <hr class="hr weather-hr" />
+                    <p class="h6 badge badge-dark">Project Tasks</p>                    
+                    <p class="text-sm-left font-italic"><small class="text-muted">${team[i].tasks}</small></p>
+                </div>
+            </div>
+        </div>`);
+
+        // Append the HTML Card
+        $("#team").append(teamCard);
+    }    
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,23 @@
 const wikipediaAPIKey = "";
 const spotifyAPIKey = "c1ec80a3b6msh5a59fdbd8715fddp1e8eb2jsn5706ec8ba70f";
 
+// Clear all HTML elements
+function clearHTML()
+{
+  // Function to empty out the jumbotron element (wikipedia facts)
+  $("#jumbotron").empty();
+
+  // Function to empty out the trendingMain element (sportify albums)
+  $("#trendingMain").empty();
+
+  // Function to empty out the trendingSide element (sportify tracks)
+  $("#trendingSide").empty();
+
+  // Function to empty out the team element
+  $("#team").empty();
+
+}
+
 // function to hide the invalid user search alert
 function alertClose() {
   document.getElementById("ms-alert").style.visibility = "hidden";
@@ -43,6 +60,10 @@ function checkSearch(userSearch) {
         // Display an alert for invalid user search
         document.getElementById("ms-alert").style.visibility = "visible";
       } else {
+
+        // Clear all HTML elements
+        clearHTML()
+
         // Save into the local storage the user search
         saveSearch(userSearch);
   
@@ -53,9 +74,8 @@ function checkSearch(userSearch) {
         // searchWikipedia(userSearch);
   
         // Display Spotify data into HTML
-        //  searchSpotify(userSearch);
-        /*      };
-            });*/
+        searchSpotify(userSearch);
+        
       }
     });
   }
@@ -82,100 +102,9 @@ $("#search-button").on("click", function (event) {
 $("#btTeam").on("click", function (event) {
   event.preventDefault();
 
-  // Function to empty out the jumbotron element (wikipedia facts)
-  $("#jumbotron").empty();
+  // Clear all HTML elements
+  clearHTML()
 
-  // Function to empty out the trendingMain element (sportify albums)
-  $("#trendingMain").empty();
-
-  // Function to empty out the trendingSide element (sportify tracks)
-  $("#trendingSide").empty();
-
-
-  // Build the html card
-  var teamCard = $(`  
-<div class="row justify-content-center" style="width: 80rem;">
-  <div class="col-sm-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title mark">Amal Hussein</h5>
-        <div class="footer-basic">
-        <div class="social">
-          <a href="#" target="_blank"><i class="icon ion-social-linkedin"></i></a>
-          <a href="#" target="_blank"><i class="icon ion-social-github"></i></a>
-          <a href="mailto:amula9986.ah@gmail.com" target="_blank"><i class="icon ion-email"></i></a>
-        </div>
-      </div>
-        <hr class="hr weather-hr" />
-        <span >Project Tasks </span>
-        <br/>
-        <span class="badge badge-primary">Implementing Wikipedia Functionality </span>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-3">
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title mark">Jack Labukas</h5>
-    <div class="footer-basic">
-    <div class="social">
-      <a href="#" target="_blank"><i class="icon ion-social-linkedin"></i></a>
-      <a href="#" target="_blank"><i class="icon ion-social-github"></i></a>
-      <a href="mailto:labukasgediminas@gmail.com" target="_blank"><i class="icon ion-email"></i></a>
-    </div>
-  </div>
-    <hr class="hr weather-hr" />
-    <span >Project Tasks </span>
-    <br/>
-    <span class="badge badge-primary">Implementing Spotify Functionality </span>
-  </div>
-</div> 
-</div>
-<div class="col-sm-3">
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title mark">Kassi</h5>
-    <div class="footer-basic">
-      <div class="social">
-        <a href="#" target="_blank"><i class="icon ion-social-linkedin"></i></a>
-        <a href="#" target="_blank"><i class="icon ion-social-github"></i></a>
-        <a href="mailto:kassy.g.e@hotmail.com" target="_blank"><i class="icon ion-email"></i></a>
-      </div>
-    </div>
-    <hr class="hr weather-hr" />
-    <span >Project Tasks </span>
-    <br/>
-    <span class="badge badge-primary">Presenting </span>
-    <span class="badge badge-warning">Assisting with Spotify API </span>
-    <span class="badge badge-success">Generating ReadMe file </span>
-    <span class="badge badge-info">Team organisation </span>
-  </div>
-</div>
-</div>
-<div class="col-sm-3">
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title mark">Rui Belo</h5>
-    <div class="footer-basic">
-      <div class="social">
-        <a href="https://linkedin.com/in/ruibelo" target="_blank"><i class="icon ion-social-linkedin"></i></a>
-        <a href="https://github.com/CZ-RBelo/" target="_blank"><i class="icon ion-social-github"></i></a>
-        <a href="mailto:jr.belo@hotmail.com" target="_blank"><i class="icon ion-email"></i></a>
-      </div>
-    </div>
-    <hr class="hr weather-hr" />
-    <span >Project Tasks </span>
-    <br/>
-    <span class="badge badge-primary">Bootstrap </span>
-    <span class="badge badge-warning">UI Design </span>
-    <span class="badge badge-success">Implementing Code/ Refactoring </span>
-    <span class="badge badge-info">Combining APIs & Local Storage </span>
-  </div>
-</div>
-</div>
-</div> 
-  `);
-
-  // Append the HTML Card
-  $("#team").append(teamCard);
+  // Display Team data into HTML
+  displayTeam()  
 });
